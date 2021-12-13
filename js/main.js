2915f8eef6487e10283b99ef8a086d86b72e3b80
vladimirKolot <89738890+vladimirKolot@users.noreply.github.com>
2021-12-13 22:07:10 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,13 +3,19 @@ $(document).ready(function () {
   var floorPath = $(".home-image path");
   var counterUp = $(".counter-up"); //кнопка увеличения этажа
   var counterDown = $(".counter-down"); //кнопка уменльшения этажа
+  var modal = $(".modal");
+  var modalCloseButton = $(".modal-close-button");
+  var viewFlatsButton = $(".view-flats")
   floorPath.on("mouseover", function () {
     floorPath.removeClass("current-floor"); //удаляем активный класс у этажей
     currentFloor = $(this).attr("data-floor"); //получаем занчение текущего этажа
     $(".counter").text(currentFloor);//записывам значение этажа в счетчик спава
   });
-
-  counterUp.on("click", function() { //отслеживаем клик по кнопке вверх
+  
+  floorPath.on("click", toggleModal);
+  modalCloseButton.on("click", toggleModal);
+  viewFlatsButton.on("click", toggleModal)
+  counterUp.on("click", function () { //отслеживаем клик по кнопке вверх
     if (currentFloor < 18) { //проверяем значение этажа, оно не должно быть больше 18
       currentFloor++ //прибавляем один этаж
       usCurrenFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false}); //форматируем переменную с этажем, чтобы было 01 а не 1
@@ -28,4 +34,7 @@ $(document).ready(function () {
       $(`[data-floor=${usCurrenFloor}]`).toggleClass("current-floor"); //подсвечиваем текущий этаж
     }
   });
+  function toggleModal() { //функция открыть-закрыть окно
+    modal.toggleClass("is-open")
+  }
 });
